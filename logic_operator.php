200f14b51b58b6056e7a33efbf7e20e0dcd975a9
ysedeveloper <ysedeveloper@gmail.com>
2020-11-30 02:29:24 +0900

    2.9.논리 연산자

---
+++ logic_operator.php
@@ -0,0 +1,13 @@
+$a = true;
+$b = false;
+
+$c = $a && $b;
+$d = $a || $b;
+$e = !$a;
+
+
+var_dump($a);echo "<br />";
+var_dump($b);echo "<br />";
+var_dump($c);echo "<br />";
+var_dump($d);echo "<br />";
+var_dump($e);echo "<br />";
