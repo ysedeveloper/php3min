99a02544b8e46ea1b6a5f4824382b43d56481e60
ysedeveloper <ysedeveloper@gmail.com>
2020-11-30 02:20:27 +0900

    변수

---
+++ var.php
@@ -0,0 +1,18 @@
+<?php
+$a = 1; // 정수 int
+$b = 2; // 정수 int
+$c = $a + $b; // 정수 계산
+$d = 3.14; // 실수 float
+$e = "hi"; // 문자열 string
+$f = 'hello'; // 문자열 string
+$g = array(1,2,3,4); // 배열 array
+$h = array('a'=>1, 'b'=> 2); // 연관배열 associative array
+
+var_dump($a);echo "<br />";
+var_dump($b);echo "<br />";
+var_dump($c);echo "<br />";
+var_dump($d);echo "<br />";
+var_dump($e);echo "<br />";
+var_dump($f);echo "<br />";
+var_dump($g);echo "<br />";
+var_dump($h);echo "<br />";
