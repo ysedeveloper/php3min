fbd3ad0c88fc93596d6f0ca4d8c2b6f40d5607e2
ysedeveloper <ysedeveloper@gmail.com>
2020-11-30 02:29:08 +0900

    2.8. 비교 연산자

---
+++ diff_operator.php
@@ -0,0 +1,30 @@
+<?php
+$a = 10;
+$b = "10";
+
+$c = $a == $b;
+$d = $a === $b;
+$e = $a != $b;
+$f = $a <> $b;
+$g = $a !== $b;
+
+$h = 20;
+
+$i = $a > $h;
+$j = $a < $h;
+$k = $a >= $h;
+$l = $a <= $h;
+
+
+var_dump($a); echo "<br />";
+var_dump($b); echo "<br />";
+var_dump($c); echo "<br />";
+var_dump($d); echo "<br />";
+var_dump($e); echo "<br />";
+var_dump($f); echo "<br />";
+var_dump($g);echo "<br />";
+var_dump($h);echo "<br />";
+var_dump($i);echo "<br />";
+var_dump($j);echo "<br />";
+var_dump($k);echo "<br />";
+var_dump($l);echo "<br />";
