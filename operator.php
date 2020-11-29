965df7ded5b356f40bfb54418812a0e6127ed9c6
ysedeveloper <ysedeveloper@gmail.com>
2020-11-30 02:28:51 +0900

    2.7. 산술 연산자와 문자열 연산자

---
+++ operator.php
@@ -0,0 +1,26 @@
+<?php
+$a = 10;
+$b = 20;
+
+$c = $a + $b;
+$d = $a - $b;
+$e = $a * $b;
+$f = $a / $b;
+$g = $a % $b;
+
+$h = 'hi ';
+$i = "php";
+$j = $h . $i;
+$k = "$h nice to meet you. $i";
+
+var_dump($a);echo "<br />";
+var_dump($b);echo "<br />";
+var_dump($c);echo "<br />";
+var_dump($d);echo "<br />";
+var_dump($e);echo "<br />";
+var_dump($f);echo "<br />";
+var_dump($g);echo "<br />";
+var_dump($h);echo "<br />";
+var_dump($i);echo "<br />";
+var_dump($j);echo "<br />";
+var_dump($k);echo "<br />";
