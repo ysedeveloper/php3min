11602d9d99919fbea2d1c2f2b43f59289635586f
ysedeveloper <ysedeveloper@gmail.com>
2020-12-05 12:35:13 +0900

    5.2. 문자열이 특정 문자열로 끝나는지 검사하기

---
+++ string_ends_with.php
@@ -0,0 +1,8 @@
+<?php
+function string_ends_with($input, $value)
+{
+    return $value === "" || (($temp = mb_strlen($input) - mb_strlen($value)) >= 0 && mb_strpos($input, $value, $temp) !== false);
+}
+
+var_dump(string_ends_with("안녕하세요.", "하세요."));
+var_dump(string_ends_with("안녕하세요.", "하이"));
