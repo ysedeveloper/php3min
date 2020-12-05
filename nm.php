4e604e786157e7911b202ea7307ab2fcf8ea1587
ysedeveloper <ysedeveloper@gmail.com>
2020-12-05 12:47:53 +0900

    7.4. 네임스페이스 사용하기

---
+++ nm.php
@@ -0,0 +1,10 @@
+<?php
+require_once("yse/sample.php");
+use yse\Sample;
+
+$sample = Sample::factory();
+$sample->tell();
+
+echo "<br />";
+$sample = new yse\Sample();
+$sample->add_age(5)->tell();
